f58de983922cf722986dbe1e30bdc148e8f6e05e
zabochen <zabochen@gmail.com>
2020-05-24 22:59:52 +0300

    StoryBoard, ViewController, Lifecycle.

---
+++ NavigationApp/ViewController/MainViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  NavigationApp
+//
+//  Created by Taras on 23.05.2020.
+//  Copyright © 2020 Taras. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func loadView() {
+        super.loadView()
+        
+        // let myView = UIView()
+        // myView.backgroundColor = UIColor.green
+        // view = myView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+    }
+ 
+    deinit {
+        // last destroy
+    }
+    
+}
