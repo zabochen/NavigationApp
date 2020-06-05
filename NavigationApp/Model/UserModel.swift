21ae570df034042e631d7361b8f4833c6b36dfd7
zabochen <zabochen@gmail.com>
2020-06-05 17:13:25 +0300

    Add TableViewController

---
+++ NavigationApp/Model/UserModel.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  NavigationApp
+//
+//  Created by Taras on 05.06.2020.
+//  Copyright © 2020 Taras. All rights reserved.
+//
+
+import Foundation
+
+struct UserModel {
+    var name: String
+    var age: Int
+}
